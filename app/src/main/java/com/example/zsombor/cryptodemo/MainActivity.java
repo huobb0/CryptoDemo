5c491cabd4e7645f4e6a1bb664b3e7a93fe3cc22
huobb0 <zsombor.kovacs@gmail.com>
2018-05-28 16:15:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/zsombor/cryptodemo/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.zsombor.cryptodemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText plain;
+    private EditText cipher;
+    private Button button;
+
+    public static String encrypt(String key, String initVector, String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            String my = new String(encrypted);
+            return my;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        plain = (EditText) findViewById(R.id.editText3);
+        cipher = (EditText) findViewById(R.id.editText4);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0){
+                String key = "Bar12345Bar12345"; // 128 bit key
+                String initVector = "RandomInitVector"; // 16 bytes IV
+                cipher.setText(encrypt(key,initVector,plain.getText().toString()));
+            }
+        });
+    }
+}
