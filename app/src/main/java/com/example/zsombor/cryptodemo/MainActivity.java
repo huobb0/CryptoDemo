da76b9058559bf60437329713679be11ff816e52
timurxyz <x@secdev.eu>
2021-02-21 06:08:39 +0100

    -- JB auto-refactored to AndroidX

--- app/src/main/java/com/example/zsombor/cryptodemo/MainActivity.java
+++ app/src/main/java/com/example/zsombor/cryptodemo/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.zsombor.cryptodemo;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
