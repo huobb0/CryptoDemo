d6de3e1f76806f00c9108b4eb789eb13a7d9e4f6
timur x <timurx@eezza.dev>
2024-11-03 03:24:34 +0100

    Gradle > 8.10.2 / 8.7.2; androidx.test.core.app.ApplicationProvider

--- app/src/androidTest/java/com/example/zsombor/cryptodemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/zsombor/cryptodemo/ExampleInstrumentedTest.java
@@ -1,7 +1,8 @@
 package com.example.zsombor.cryptodemo;
 
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
+// import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
@@ -19,7 +20,8 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        // Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = ApplicationProvider.getApplicationContext();
 
         assertEquals("com.example.zsombor.cryptodemo", appContext.getPackageName());
     }
